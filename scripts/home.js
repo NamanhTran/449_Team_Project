d8cbfd959b92c2d515658adaad53c80f8a4503db
Namanh Tran <NamanhDTran@gmail.com>
2021-11-09 00:27:57 -0800

    reorganized project

--- scripts/home.js
+++
@@ -1,7 +0,0 @@
-window.onload = async function(event) {
-    const SERVER_URL = "http://localhost:3000";
-    const { data } = await axios.get(SERVER_URL + '/trending');
-
-    console.log(data);
-    event.preventDefault();
-};
